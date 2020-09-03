78cfaf3ab5c1b252f5ebc4e6d53246f3b3a402d5
CeKing2311 <593537367@qq.com>
2020-09-03 14:29:02 +0800

    完成商品功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,9 @@ import Rights from '../components/power/Rights.vue';
 import Roles from '../components/power/Roles';
 import Cate from '../components/goods/Cate';
 import Params from '../components/goods/Params';
+import GoodsList from '../components/goods/List';
+import AddGoods from '../components/goods/Add';
+
 Vue.use(VueRouter);
 
 const routes = [{
@@ -38,6 +41,12 @@ const routes = [{
   }, {
     path: '/params',
     component: Params
+  }, {
+    path: '/goods',
+    component: GoodsList
+  }, {
+    path: '/goods/add',
+    component: AddGoods
   }]
 }];
 
